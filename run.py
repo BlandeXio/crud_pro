31f683f5a6d235130cd00c88655885f82ab9627b
louis <llsmingyi@163.com>
2018-03-07 22:43:45 +0800

    homepage finished

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import os
 
 from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG')
-# config_name = 'development'
+# config_name = os.getenv('FLASK_CONFIG')
+config_name = 'development'
 app = create_app(config_name)
 
 if __name__ == '__main__':
