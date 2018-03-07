da05236e527595e276258538c5f96ec1a0b3c30b
louis <llsmingyi@163.com>
2018-03-07 21:31:00 +0800

    init repo

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# local imports
+from config import app_config
+from flask_login import LoginManager
+from flask_migrate import Migrate
+# db variable initialization
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app(config_name):
+    # existing code remains
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
+
+    migrate = Migrate(app, db)
+
+    from app import models
+    return app
