070920af02e951ba13c34888691628abf7e411ab
louis <llsmingyi@163.com>
2018-03-10 20:21:10 +0800

    employees module finished

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
+from wtforms.validators import DataRequired
+from ..models import Department, Role
 class DepartmentForm(FlaskForm):
     """
     Form for admin to add or edit a department
@@ -16,4 +18,12 @@ class RoleForm(FlaskForm):
     """
     name = StringField('Name', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class EmployeeAssignForm(FlaskForm):
+    """
+    Form for admin to assign departments and roles to employees
+    """
+    department = QuerySelectField(query_factory=lambda: Department.query.all(), get_pk=lambda  x: "id", get_label="name")
+    role = QuerySelectField(query_factory=lambda: Role.query.all(), get_pk=lambda  x: "id", get_label="name")
     submit = SubmitField('Submit')
